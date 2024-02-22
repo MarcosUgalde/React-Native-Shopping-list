ea370534e917a9c7f5db92e133d3a482064aa7b3
Marcos <marcosugaldemartinez@gmail.com>
2024-02-22 16:35:46 +0100

    feat: imported navbar and settings components into app

--- Shopping-list/App.js
+++ Shopping-list/App.js
@@ -1,11 +1,36 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import List from "./components/List";
+import { useState } from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Settings from "./components/Settings";
+
+const tabs = ["List", "Settings"];
 
 export default function App() {
+  const [activeTab, setActiveTab] = useState("List");
+
+  const handleTabPress = (tabName) => {
+    setActiveTab(tabName);
+  };
+
+  const renderActiveComponent = () => {
+    switch (activeTab) {
+      case "List":
+        return <List />;
+      case "Settings":
+        return <Settings />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <List />
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        {renderActiveComponent()}
+      </View>
+      <Navbar tabs={tabs} activeTab={activeTab} onTabPress={handleTabPress} />
       <StatusBar style="auto" />
     </View>
   );
