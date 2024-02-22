c23f1009815ce8181239bfe97bb1f497186c2bba
Marcos <marcosugaldemartinez@gmail.com>
2024-02-22 16:34:12 +0100

    feat: created settings page

---
+++ Shopping-list/components/Settings/index.js
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+function Settings() {
+  return (
+    <View>
+      <Text>Settings</Text>
+      <Text>A paragraph explaining something</Text>
+    </View>
+  );
+}
+
+export default Settings;
